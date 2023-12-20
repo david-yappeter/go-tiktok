08b6b1485be3337ed699850b9f24c7ddf5b84f2c
louisaldorio <louisaldorio@gmail.com>
2023-12-20 15:56:56 +0700

    get pacfkage detail

--- fulfillment.go
+++ fulfillment.go
@@ -20,3 +20,19 @@ func (c *Client) GetTracking(ctx context.Context, p CommonParam, orderId string)
 	)
 	return
 }
+
+// GetTracking get tracking.
+func (c *Client) GetPackageDetail(ctx context.Context, p CommonParam, packageId string) (pack Package, err error) {
+	param, err := c.commonParam(p)
+	if err != nil {
+		return
+	}
+
+	err = c.Get(
+		ctx,
+		fmt.Sprintf("/fulfillment/%s/packages/%s", c.version, packageId),
+		param,
+		&pack,
+	)
+	return
+}
--- fulfillment_.go
+++ fulfillment_.go
@@ -8,3 +8,21 @@ type Tracking struct {
 type TrackingList struct {
 	Trackings []Tracking `json:"trackings"`
 }
+
+type PackageOrderSku struct {
+	Id       string `json:"id"`
+	ImageUrl string `json:"image_url"`
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
+}
+
+type PackageOrder struct {
+	Id   string            `json:"id"`
+	Skus []PackageOrderSku `json:"skus"`
+}
+
+type Package struct {
+	Id     string            `json:"package_id"`
+	Status string            `json:"package_status"`
+	Orders []PackageOrderSku `json:"orders"`
+}
